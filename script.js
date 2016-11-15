08f52744978fa83717956da9d96cf01c1dcbdb90
Y.Y Chen <Y.Y Chen>
2016-11-15 09:49:54 +0800

    add js

---
+++ script.js
@@ -0,0 +1,44 @@
+function set()
+{
+}
+
+function btnInit()
+{
+	var btnList = document.getElementsByTagName("button");
+	fot(i = 0;i <btnList.length;i++)
+	{
+		if(btnList[i].innerHTML == "")
+			btnList[i].disabled = true;
+		else
+			btnList[i].addEventListener("click",btnClicked);
+	}
+}
+
+function btnClicked(event)
+{
+}
+
+function baseInit()
+{
+	
+}
+
+function changeScreenBase(event)
+{
+}
+
+var currentNum = {
+}
+
+function convert()
+{
+}
+
+var screenDis = {
+}
+
+var btnHandle = {
+}
+
+var expHandle = {
+}
